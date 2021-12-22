0976031cd3c151482c296afb0b3ad9380982a32d
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-12-22 13:58:03 -0300

    Fix variables2.rs.

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,10 +1,13 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
+//Data Types
+//https://doc.rust-lang.org/book/ch03-02-data-types.html
 
 fn main() {
-    let x;
+    let x = 10; //infer the type
+    //let x: i32 = 11; //explicitly say the type
+    //let x: char = 'A'; //explicitly say a type different from 10 which causes a 'mismatched' types error
     if x == 10 {
         println!("Ten!");
     } else {
